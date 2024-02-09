39252feddca07327677f462b1bca8982aa1e11fb
vitortakeshi <vitortakeshi2003@gmail.com>
2024-02-09 01:29:31 -0300

    Criando o sroteador

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,6 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+}
