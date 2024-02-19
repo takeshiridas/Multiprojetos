0c36f67048fac7fa054158051588204827011d5d
vitortakeshi <vitortakeshi2003@gmail.com>
2024-02-19 11:38:53 -0300

    funcao alterarStatusBotao

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -3,8 +3,38 @@ function sortear() {
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    
+    let sorteados = [];
+    let numero;
+
+    for(let i = 0; i < quantidade; i++) {
+        numero = gerarNumeroAleatorio(de, ate);
+
+        while(sorteados.includes(numero)) {
+            numero = gerarNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+
+    alterarStatusBotao();
 }
 
-function gerarNumeroAleatorio {
-    
+function gerarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }
+    else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+        
+    }
 }
