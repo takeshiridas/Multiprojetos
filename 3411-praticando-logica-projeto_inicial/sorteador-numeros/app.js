169daaeb951edf19ea488fcbef31dc05454432f6
vitortakeshi <vitortakeshi2003@gmail.com>
2024-02-19 11:59:06 -0300

    falta bloquear o botao sortear dps da primeira tentativa

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -32,9 +32,18 @@ function alterarStatusBotao() {
         botao.classList.remove('container__botao-desabilitado');
         botao.classList.add('container__botao');
     }
+
     else{
         botao.classList.remove('container__botao');
-        botao.classList.add('container__botao-desabilitado');
-        
+        botao.classList.add('container__botao-desabilitado');        
     }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    
+    alterarStatusBotao();
 }
