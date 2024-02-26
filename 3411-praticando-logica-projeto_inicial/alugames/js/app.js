4ffb159d01070f7d0b8a96b632092113b97d0f7b
vitortakeshi <vitortakeshi2003@gmail.com>
2024-02-26 00:42:27 -0300

    Confirmacao de devolucao de jogo

--- 3411-praticando-logica-projeto_inicial/alugames/js/app.js
+++ 3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -2,13 +2,16 @@ function alterarStatus(id) {
     let jogoClicado = document.getElementById(`game-${id}`);
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
     let botao = jogoClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
 
-    if(imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
-    }
-    else {
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
+    } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
